2ecc7af1d177319a36c07572a536d09c33c79422
magistermaks <53909807+magistermaks@users.noreply.github.com>
2020-11-23 17:06:45 +0100

    Better (?)

--- SKARBFI.cpp
+++ SKARBFI.cpp
@@ -10,63 +10,39 @@
 #define EAST 3
 #define SECRET_WORD "studnia"
 
-#define STR_2( macro ) #macro
-#define STR( macro ) STR_2( macro )
+#define __STR( macro ) #macro
+#define STR( macro ) __STR( macro )
+#define BEGIN void test(); int main() { size_t t; std::cin >> t; while ( t --> 0 ) test(); } void test()
 
-int main(){
+BEGIN {
 
-	size_t t;
-	std::cin >> t;
+	long n = 0, x = 0, y = 0;
+	std::cin >> n;
 
-	while( t --> 0 ){
+	while( n --> 0 ) {
 
-		size_t n;
-		std::cin >> n;
+		long a, b;
+		std::cin >> a >> b;
 
-		long x = 0, y = 0;
-
-		while( n --> 0 ) {
-
-			long a, b;
-			std::cin >> a >> b;
-
-			switch( a ) {
-
-				case NORTH:
-					y += b;
-					break;
-
-				case SOUTH:
-					y -= b;
-					break;
-
-				case WEST:
-					x -= b;
-					break;
-
-				case EAST:
-					x += b;
-					break;
-
-			}
-
-		}
-
-		if( x == 0 && y == 0 ) {
-			std::cout << SECRET_WORD << std::endl;
-		}else{
-
-			if( y != 0 ) {
-				std::cout << (y > 0 ? STR(NORTH) : STR(SOUTH)) << " " << std::abs(y) << std::endl;
-			}
-
-			if( x != 0 ) {
-				std::cout << (x > 0 ? STR(EAST) : STR(WEST)) << " " << std::abs(x) << std::endl;
-			}
-
+		switch( a ) {
+			case NORTH: y += b; break;
+			case SOUTH: y -= b; break;
+			case WEST: x -= b; break;
+			case EAST: x += b; break;
 		}
 
 	}
 
-	return 0;
+	if( x == 0 && y == 0 ) {
+		std::cout << SECRET_WORD << std::endl;
+	}
+
+	if( y != 0 ) {
+		std::cout << (y > 0 ? STR(NORTH) : STR(SOUTH)) << " " << std::abs(y) << std::endl;
+	}
+
+	if( x != 0 ) {
+		std::cout << (x > 0 ? STR(EAST) : STR(WEST)) << " " << std::abs(x) << std::endl;
+	}
+
 }
