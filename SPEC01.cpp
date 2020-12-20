75e6ac88ebfdc68579d899d1ac5cfcf09c8c2b8f
magistermaks <53909807+magistermaks@users.noreply.github.com>
2020-12-21 00:59:50 +0100

    Updated SPEC01

--- SPEC01.cpp
+++ SPEC01.cpp
@@ -7,18 +7,16 @@ int main() {
 
     int a[n][n] = {0}, sum = 0;
 
-    for( int y = 0; y < n; y++ ) {
-        for( int x = 0; x < n; x++ ) {
+    for( int y = 0, x = 0; y < n; x < n - 1 ? x ++ : y +++ (x = 0) ) {
 
-            std::cin >> a[x][y];
+    	std::cin >> a[x][y];
 
-            for( int y1 = y, x1 = x; y1 >= 0; x1 > 0 ? x1 -- : y1 --- (x1 = x) ) {
-            	for( int y2 = y1, x2 = x1; y2 <= y; x2 < x ? x2 ++ : y2 +++ (x2 = x1) ) {
-            		sum += a[x2][y2];
-            	}
-            }
+    	for( int y1 = y, x1 = x; y1 >= 0; x1 > 0 ? x1 -- : y1 --- (x1 = x) ) {
+    		for( int y2 = y1, x2 = x1; y2 <= y; x2 < x ? x2 ++ : y2 +++ (x2 = x1) ) {
+    			sum += a[x2][y2];
+    		}
+    	}
 
-        }
     }
 
     std::cout << sum;
