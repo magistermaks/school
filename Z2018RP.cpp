208b9829fd81b6298d26648e86c48295fe1d7993
magistermaks <53909807+magistermaks@users.noreply.github.com>
2021-02-17 23:33:55 +0100

    Fixed formatting

--- Z2018RP.cpp
+++ Z2018RP.cpp
@@ -8,17 +8,17 @@
 int max = 1;
 
 int lis( int arr[], int n) {
-	if (n == 1) return 1;
+	if( n == 1 ) return 1;
 	int a, b = 1;
 
-	for (int i = 1; i < n; i++) {
+	for( int i = 1; i < n; i++ ) {
 		a = lis(arr, i);
-		if (arr[i - 1] < arr[n - 1] && a + 1 > b) {
+		if( arr[i - 1] < arr[n - 1] && a + 1 > b ) {
             b = a + 1;
 		}
 	}
 
-	if (max < b) max = b;
+	if( max < b ) max = b;
     return b;
 }
 
